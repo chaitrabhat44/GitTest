824e6c0bafcc8147a74e4b21cb50ee63a68592f0
Chaitra Bhat <chaitrabhat44@gmail.com>
2014-01-18 14:15:11 -0700

    my First Commit

---
+++ Hello.java
@@ -0,0 +1,9 @@
+/**
+ * The Hello class implements an application that
+ * simply prints a string to standard output.
+ */
+class Hello {
+    public static void main(String[] args) {
+        System.out.println("Yippie!! My Git Repository is working!!"); // Display the string.
+    }
+}
